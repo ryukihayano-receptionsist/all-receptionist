b793b57916a21e335f4804ea357633292756b0c1
Hayano Ryuki <hayano@media-confidence.com>
2026-03-05 14:14:57 +0900

    修正⑥⑦: フッターサポート項目追加・プロダクトリンク統一

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
             </div>
             <div className="uk-width-5-6@m uk-flex" style={{ flexWrap: 'wrap', gap: '0' }}>
               {/* RECEPTIONIST */}
-              <div className="uk-width-1-4@m">
+              <div className="f-nav-col">
                 <div className="tit-box">
                   <h6>
                     <a href="https://receptionist.jp/">
@@ -26,59 +26,82 @@ export default function Footer() {
                 </div>
                 <ul className="f-nav-list">
                   <li><a href="https://receptionist.jp/function">機能の紹介</a></li>
-                  <li><a href="https://receptionist.jp/case">お客様の声</a></li>
-                  <li><a href="https://receptionist.jp/plan">料金プラン</a></li>
-                  <li><a href="https://receptionist.jp/faq">よくあるご質問</a></li>
-                  <li><a href="https://receptionist.jp/contact-select">お問い合わせ</a></li>
-                  <li><a href="https://app.receptionist.jp/sign_in">ログイン</a></li>
-                  <li><a href="https://receptionist.jp/document">資料請求</a></li>
-                </ul>
-              </div>
-              {/* 調整アポ */}
-              <div className="uk-width-1-4@m">
-                <div className="tit-box">
-                  <h6>
-                    <a href="https://scheduling.receptionist.jp">
-                      <small>日程調整ツール</small>調整アポ
-                    </a>
-                  </h6>
-                </div>
-                <ul className="f-nav-list">
-                  <li><a href="https://scheduling.receptionist.jp/function">機能のご紹介</a></li>
-                  <li><a href="https://scheduling.receptionist.jp/case">お客様の声</a></li>
-                  <li><a href="https://scheduling.receptionist.jp/plan">料金プラン</a></li>
-                  <li><a href="https://scheduling.receptionist.jp/faq">よくあるご質問</a></li>
-                  <li><a href="https://scheduling.receptionist.jp/contact">お問い合わせ</a></li>
-                  <li><a href="https://scheduling.receptionist.jp/document">資料請求</a></li>
-                  <li><a href="https://scheduling.receptionist.jp/document-price">料金表</a></li>
-                  <li><a href="https://scheduling.receptionist.jp/howto/">使い方</a></li>
-                  <li><a href="https://scheduling.receptionist.jp/blog/">ブログ</a></li>
-                  <li><a href="https://scheduling.app.receptionist.jp/sign_in">ログイン</a></li>
+                  <li><a href="https://receptionist.jp/case">導入事例</a></li>
+                  <li><a href="https://receptionist.jp/plan">料金表</a></li>
+                  <li><a href="https://receptionist.jp/document">資料一覧</a></li>
                 </ul>
               </div>
               {/* 予約ルームズ */}
-              <div className="uk-width-1-4@m">
+              <div className="f-nav-col">
                 <div className="tit-box">
                   <h6>
-                    <a href="https://rooms.receptionist.jp">
+                    <a href="https://rooms.receptionist.jp/">
                       <small>会議室の予約管理システム</small>予約ルームズ
                     </a>
                   </h6>
                 </div>
                 <ul className="f-nav-list">
-                  <li><a href="https://rooms.receptionist.jp/function/">機能のご紹介</a></li>
-                  <li><a href="https://rooms.receptionist.jp/document-price/">プラン</a></li>
-                  <li><a href="https://rooms.receptionist.jp/contact">お問い合わせ</a></li>
-                  <li><a href="https://rooms.receptionist.jp/documents/">資料請求</a></li>
+                  <li><a href="https://rooms.receptionist.jp/function/">機能の紹介</a></li>
+                  <li><a href="https://rooms.receptionist.jp/case/">導入事例</a></li>
+                  <li><a href="https://rooms.receptionist.jp/document-price/">料金表</a></li>
+                  <li><a href="https://rooms.receptionist.jp/documents/">資料</a></li>
+                </ul>
+              </div>
+              {/* 調整アポ */}
+              <div className="f-nav-col">
+                <div className="tit-box">
+                  <h6>
+                    <a href="https://scheduling.receptionist.jp/">
+                      <small>日程調整ツール</small>調整アポ
+                    </a>
+                  </h6>
+                </div>
+                <ul className="f-nav-list">
+                  <li><a href="https://scheduling.receptionist.jp/function">機能の紹介</a></li>
+                  <li><a href="https://scheduling.receptionist.jp/case">導入事例</a></li>
+                  <li><a href="https://scheduling.receptionist.jp/plan">料金表</a></li>
+                  <li><a href="https://scheduling.receptionist.jp/document">資料</a></li>
+                </ul>
+              </div>
+              {/* 招待レセプション */}
+              <div className="f-nav-col">
+                <div className="tit-box">
+                  <h6>
+                    <a href="https://event.receptionist.jp/">
+                      <small>イベント受付システム</small>招待レセプション
+                    </a>
+                  </h6>
+                </div>
+                <ul className="f-nav-list">
+                  <li><a href="https://event.receptionist.jp/function/">機能の紹介</a></li>
+                  <li><a href="https://event.receptionist.jp/case/">導入事例</a></li>
+                  <li><a href="https://event.receptionist.jp/document/">資料</a></li>
+                </ul>
+              </div>
+              {/* 代表電話コネクト */}
+              <div className="f-nav-col">
+                <div className="tit-box">
+                  <h6>
+                    <a href="https://ivr.receptionist.jp/">
+                      <small>代表電話の自動応答</small>代表電話コネクト
+                    </a>
+                  </h6>
+                </div>
+                <ul className="f-nav-list">
+                  <li><a href="https://ivr.receptionist.jp/function/">機能の紹介</a></li>
+                  <li><a href="https://ivr.receptionist.jp/case/">導入事例</a></li>
+                  <li><a href="https://ivr.receptionist.jp/document/">資料</a></li>
                 </ul>
               </div>
               {/* サポート */}
-              <div className="uk-width-1-4@m">
+              <div className="f-nav-col">
                 <div className="tit-box">
                   <h6><small className="pc">&nbsp;</small>サポート</h6>
                 </div>
                 <ul className="f-nav-list">
-                  <li><a href="/event/">イベント情報</a></li>
+                  <li><a href="https://app.receptionist.jp/sign_in">ログイン</a></li>
+                  <li><a href="https://receptionist.jp/seminar/">講習会</a></li>
+                  <li><a href="https://help.receptionist.jp/">ヘルプサイト</a></li>
                 </ul>
               </div>
               {/* 下部リンク */}
